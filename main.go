79e19a97e890ab85249ff9bbd5d0cb2252354d42
tnk-rin <47429850+tnk-rin@users.noreply.github.com>
2022-06-11 18:46:01 -0700

    temperatures shown in ui

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
+	"net"
 	"net/http"
-	"hddtemp"
+	"fmt"
+	"strings"
+	"bufio"
 	"github.com/gin-gonic/gin"
+	"strconv"
+	"unicode/utf8"
 )
 
 func main() {
-
-	hddtemp.Temp("/dev/sda");
-
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
+
+	router.Static("/static", "./views")
+
+	router.LoadHTMLGlob("views/html/*")
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H {
+		c.HTML(http.StatusOK, "index.html", gin.H {
 			"title": "Dashboard",
+			"sdaTemp": Temp("/dev/sda"),
+			"sdbTemp": Temp("/dev/sdb"),
+			"sdcTemp": Temp("/dev/sdc"),
 		})
 	})
 
@@ -24,4 +32,30 @@ func main() {
 		})
 	})
 	router.Run()
+}
+
+func Temp(drive string) int {
+	c, err := net.Dial("tcp", "localhost:7634")
+	if err != nil {
+		fmt.Println(err)
+		return -1
+	}
+
+	reply, err := bufio.NewReader(c).ReadString('\n')
+	status := Trim(reply)
+	s := strings.Split(status, "||")
+	t := 0
+	for _, j := range s {
+		d := strings.Split(j, "|")
+		if d[0] == drive {
+			t, _ = strconv.Atoi(d[2])
+		}
+	}
+
+	return t
+}
+
+func Trim(s string) string {
+	_, i := utf8.DecodeRuneInString(s)
+	return s[i:]
 }
